a4d3cadc2b88c7d24df0cf3039e36435b648b6a1
chaseplayz180 <chaseplayz180@gmail.com>
2025-04-05 22:29:41 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,8 +33,8 @@ else
     echo "13. Minecraft 1.17.1"
     echo "14. Minecraft 1.18.1"
     echo "15. Minecraft 1.19.1"
-    echo "16. Minecraft 1.20.1"
-    read -p "Enter your choice (1-16): " version_choice
+    echo "16. Minecraft script.sh"
+    read -p "Enter your choice (1.20.1): " version_choice
 
     case "$version_choice" in
         1 ) minecraft_version="1.5.2";;
